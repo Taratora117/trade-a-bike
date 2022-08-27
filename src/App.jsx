b39cdcfb00b7475a848cdac83dd170341824b243
Taratora117 <alex.j.hkrystev@gmail.com>
2022-08-27 20:27:57 +0200

    added packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Routes>
+      <Route index element={<Navigate replace to="home" />} />
+      <Route path="home" element={<Home />} />
+      <Route path="*" />
+    </Routes>
   );
 }
 
 export default App;
+
