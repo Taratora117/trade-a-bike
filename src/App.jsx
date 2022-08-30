f99348275659ea04265642da9f4d1de7c48b7c82
Taratora117 <alex.j.hkrystev@gmail.com>
2022-08-30 17:04:28 +0200

    Added Login and Register Close #5

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Home from './pages/Home';
 import './App.css';
 import Page from './components/Page/Page';
 import AppContext from './context/AppContext';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   const [appState, setAppState] = useState({
@@ -18,6 +20,8 @@ function App() {
       <Routes>
         <Route index element={<Navigate replace to="home" />} />
         <Route path="/home" element={<Page><Home /></Page>} />
+        <Route path="/login" element={<Page><Login /></Page>} />
+        <Route path="/register" element={<Page><Register /></Page>} />
         <Route path="*" />
       </Routes>
     </AppContext.Provider>
