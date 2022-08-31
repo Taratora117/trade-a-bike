8501f641815fa6dea5340db8be63e736c88bcc49
Taratora117 <alex.j.hkrystev@gmail.com>
2022-08-31 19:21:40 +0200

    Added home template

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,52 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='container my-5'>
+      <div className='row mb-3'>
+        <div className='col border border-2'>
+          <h3>New Search</h3>
+          <br />
+          <br />
+          <br />
+          <br />
+        </div>
+      </div>
+      <div className='row d-flex justify-content-around'>
+        <div className='col-3 border border-2'>
+          <p>Options</p>
+          <p>Options</p>
+          <p>Options</p>
+          <p>Options</p>
+          <p>Options</p>
+          <p>Options</p>
+          <p>Options</p>
+          <p>Options</p>
+          <p>Options</p>
+          <p>Options</p>
+          <p>Options</p>
+          <p>Options</p>
+          <p>Options</p>
+          <p>Options</p>
+        </div>
+        <div className='col-8 border border-2'>
+          <p>Content</p>
+          <p>Content</p>
+          <p>Content</p>
+          <p>Content</p>
+          <p>Content</p>
+          <p>Content</p>
+          <p>Content</p>
+          <p>Content</p>
+          <p>Content</p>
+          <p>Content</p>
+          <p>Content</p>
+          <p>Content</p>
+          <p>Content</p>
+          <p>Content</p>
+        </div>
+      </div>
+
+    </div>
   );
 };
 
