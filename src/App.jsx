f8efb3f2549009d91672abbdd460e08ef2e56283
Taratora117 <alex.j.hkrystev@gmail.com>
2022-09-02 19:00:02 +0200

    added folders for pages / added about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 // Pages
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
 import Page from './components/Page/Page';
 import AppContext from './context/AppContext';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Search from './pages/Search';
-import Categories from './pages/Categories';
-import Saved from './pages/Saved';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Search from './pages/Search/Search';
+import Categories from './pages/Categories/Categories';
+import Saved from './pages/Saved/Saved';
+import About from './pages/About/About';
 
 import './App.css';
 
@@ -28,6 +29,7 @@ function App() {
         <Route path="/search" element={<Page><Search /></Page>} />
         <Route path="/categories" element={<Page><Categories /></Page>} />
         <Route path="/saved" element={<Page><Saved /></Page>} />
+        <Route path="/about" element={<Page><About /></Page>} />
         <Route path="/login" element={<Page><Login /></Page>} />
         <Route path="/register" element={<Page><Register /></Page>} />
         <Route path="*" />
