b592f3566fdc3f3d4f37ca991808721d4a87bb11
Taratora117 <alex.j.hkrystev@gmail.com>
2022-09-03 21:52:04 +0200

    switched login and register and added login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,28 +4,12 @@ const Login = () => {
   return (
     <div className='container w-50 p-5 bg-secondary'>
       <div className='row'>
-        <label className='mb-2' htmlFor='first_name'>First Name</label>
-        <input className='form-control' id='first_name' type="text" />
-      </div>
-      <div className='row'>
-        <label className='mb-2' htmlFor='last_name'>Last Name</label>
-        <input className='form-control' id='last_name' type="text" />
-      </div>
-
-      <div className='row'>
-
-
         <label className='mb-2' htmlFor='email'>Email</label>
         <input className='form-control' id='email' type="email" />
-
       </div>
       <div className='row'>
-
-
         <label className='mb-2' htmlFor='password'>Password</label>
         <input className='form-control' id='password' type="password" />
-
-
       </div>
     </div>
   );
