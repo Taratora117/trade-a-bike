647b8a29b0a468c47c849c2be54cf5d5e03e1964
Taratora117 <alex.j.hkrystev@gmail.com>
2022-09-05 15:47:31 +0200

    added register form

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,27 +1,48 @@
 import React from 'react';
+import { useState } from 'react';
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Register = () => {
+  const [data, setData] = useState({email: '', password: ''});
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // TODO add register service
+  };
+
   return (
-    <div className='container w-50 p-5 bg-secondary'>
-      <div className='row'>
-        <label className='mb-2' htmlFor='first_name'>First Name</label>
-        <input className='form-control' id='first_name' type="text" />
-      </div>
+    <div className='container w-50 p-5'>
+      <Form onSubmit={submitHandler} className='p-5 border border-5 border-warning rounded bg-secondary'>
 
-      <div className='row'>
-        <label className='mb-2' htmlFor='last_name'>Last Name</label>
-        <input className='form-control' id='last_name' type="text" />
-      </div>
+        <Form.Group className="mb-3">
+          <Form.Label>First Name:</Form.Label>
+          <Form.Control type="email" placeholder="Email" required value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
+        </Form.Group>
 
-      <div className='row'>
-        <label className='mb-2' htmlFor='email'>Email</label>
-        <input className='form-control' id='email' type="email" />
-      </div>
+        <Form.Group className="mb-3">
+          <Form.Label>Last Name:</Form.Label>
+          <Form.Control type="email" placeholder="Email" required value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
+        </Form.Group>
 
-      <div className='row'>
-        <label className='mb-2' htmlFor='password'>Password</label>
-        <input className='form-control' id='password' type="password" />
-      </div>
+        <Form.Group className="mb-3">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control type="email" placeholder="Email" required value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control type="password" placeholder="Password" required value={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Confirm Password:</Form.Label>
+          <Form.Control type="password" placeholder="Password" required value={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">Register</Button>
+      </Form>
     </div>
   );
 };
